a91b9b77b638f4593c0d6420e086fce51b39d9c5
Purna Peramune <purnaperamune@gmail.com>
2025-10-01 22:14:58 +1000

    Action types for logging

---
+++ RMITCareHome/src/ActionLog.java
@@ -0,0 +1,17 @@
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public class ActionLog implements Serializable {
+    private final LocalDateTime when;
+    private final ActionType type;
+    private final String staffId;
+    private final String info;
+
+    private ActionLog(LocalDateTime when, ActionType type, String staffId, String info) {
+        this.when = when; this.type = type; this.staffId = staffId; this.info = info;
+    }
+    public static ActionLog now(ActionType t, String staffId, String info) {
+        return new ActionLog(LocalDateTime.now(), t, staffId, info);
+    }
+    @Override public String toString() { return when + " [" + type + "] " + staffId + " :: " + info; }
+}
---
+++ RMITCareHome/src/ActionType.java
@@ -0,0 +1,5 @@
+import java.io.Serializable;
+
+public enum ActionType implements Serializable {
+    ADD_RESIDENT, MOVE_RESIDENT, ADD_PRESCRIPTION, ADMINISTER_MED, DISCHARGE
+}
